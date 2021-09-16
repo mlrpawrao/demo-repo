1f974b47cbc2050b6cf66e18da8afccf009d502e
Pawan Kumar Rao <mlrpawrao@gmail.com>
2021-09-16 20:57:46 +0530

    added code changes to madlibs

--- 1_madlibs.py
+++ 1_madlibs.py
@@ -3,5 +3,6 @@ exclamation=input("Enter an exclamation expression: -")
 adverb=input("Enter a verb/adverb: -")
 noun=input("Enter a noun: -")
 adjective=input("Enter an adjective: -")
+print(" You have entered all the required fields for madlibs")
 
 print(f"{exclamation}! he said {adverb} as he jumped into his convertible {noun} and drove off with his {adjective}.")
